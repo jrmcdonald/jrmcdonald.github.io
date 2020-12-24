9108e0921aad337e85309206d18e6ac3edc31b89
jrmcdonald <jrmcdonald@users.noreply.github.com>
2020-12-24 08:37:30 +0000

    deployed via Gatsby Publish Action 🎩 for 34fd773603db797cf5788256a41406a27ea12994

---
+++ component---src-pages-404-js-7caf974660219c9f4e3d.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{w2l6:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),u=n("1Qp6");t.default=function(){return l.a.createElement(u.a,null,l.a.createElement(u.b,{title:"404: Not found"}),l.a.createElement("h2",null,"NOT FOUND"),l.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-7caf974660219c9f4e3d.js.map
