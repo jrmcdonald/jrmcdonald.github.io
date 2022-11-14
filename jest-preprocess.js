04a3b6e89e2f9c90e8cbcee47a00fb5c69e712ed
Jamie McDonald <jamie@qwyck.net>
2022-11-14 11:03:19 +0000

    feat: replace gatsby with simple page

--- jest-preprocess.js
+++
@@ -1,5 +0,0 @@
-const babelOptions = {
-  presets: ['babel-preset-gatsby'],
-};
-// eslint-disable-next-line import/no-extraneous-dependencies
-module.exports = require('babel-jest').createTransformer(babelOptions);
--- src/components/common/index.js
+++
@@ -1,3 +0,0 @@
-export * from './Container';
-export * from './Layout';
-export * from './SEO';
